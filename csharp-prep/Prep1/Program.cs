ac0d74b764b1c56e9355a34ec09db86cf188c40e
robertggovias <goviasuarez@gmail.com>
2025-01-09 21:15:29 -0300

    C# is great I love it, was easy to create this question game of james bond, it's working fine

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Text.Json.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //2 question - 2 answers and finally James bond answer
+        Console.WriteLine("What is your first name?");
+        string fname = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+
+
     }
 }
