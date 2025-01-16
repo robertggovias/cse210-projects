35fb6c449d6921c0d97067d62374cdd2988ae7fd
robertggovias <goviasuarez@gmail.com>
2025-01-16 19:28:58 -0300

    Prep 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> list = new List<int>();       
+        
+        
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        string newNumber;
+        do {
+            Console.WriteLine("Enter a number: ");
+            newNumber = Console.ReadLine();
+            list.Add(int.Parse(newNumber));
+        }
+        while (newNumber != "0");
+
+        int sumaLista = list.Sum();
+
+        Double averageLista = list.Average();
+
+        int MaxLista = list.Max();
+        
+        
+        Console.WriteLine($"The sum is: {sumaLista}");
+        Console.WriteLine($"The average is: {averageLista}");
+        Console.WriteLine($"The largest number is: {MaxLista}");
+
+        
+
+        list.Sort();
+
+        int minimal = 0;
+
+        foreach (int item in list){            
+            if (item <= 0) {
+                minimal++;
+            }
+            else {
+                
+            }            
+        }
+        Console.WriteLine(minimal);
+
+        Console.WriteLine($" The smallest positive number is: {list[minimal]}");
+
+        //do while con i para verificar si es negativo
+
+        foreach (int item in list){            
+            Console.WriteLine(item);
+        }
+
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,22 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {         
+        int suma;
+
+        double BrainBreak (double forSquare)
+        {
+            return Math.Pow (forSquare, 2);
+        }
+        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine("Please enter your name: ");        
+        string nombre = Console.ReadLine ();  
+        Console.WriteLine ("Please enter your favorite number: ");
+        string number = Console.ReadLine ();
+        int numero = int.Parse (number);
+
+        double squareResult = BrainBreak (numero);
+        
+        Console.WriteLine ($"Brother {nombre}, the square of your number {numero} is {squareResult}");
     }
 }
