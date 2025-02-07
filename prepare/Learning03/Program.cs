c4dba710d59566392cf7dc73f1150f7f084cb8ec
robertggovias <goviasuarez@gmail.com>
2025-02-07 02:16:16 -0300

    Learning activity fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Fraction fraction= new Fraction();
+        Console.WriteLine(fraction.GetDecimalValue());
+        Fraction fraction1= new Fraction(120,40);
+        
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        fraction1.SetTop(80);
+        Console.WriteLine(fraction.GetDecimalValue());
+        fraction.SetBottom(80);
+        Console.WriteLine(fraction.GetDecimalValue());
+        Fraction fraction2= new Fraction(120);
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
     }
-}
+    }
