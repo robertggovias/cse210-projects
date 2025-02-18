0cf85ca21e9951e0d05551d29eed27977eb1b6d3
robertggovias <goviasuarez@gmail.com>
2025-02-18 11:58:30 -0300

    works

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,17 +8,26 @@ class Program
         Console.Clear();
         Scripture scriptureTest = new Scripture("Fear thou not; for I am with thee: be not dismayed; for I am thy God: I will strengthen thee; yea, I will help thee; yea, I will uphold thee with the right hand of my righteousness.");
         Reference referenceTest = new Reference("Isaiah",41,1,3);                        
-        Word weird =  new Word("test");
+        //Word weird =  new Word("test");
         Console.WriteLine(referenceTest.GetReference() + " " + scriptureTest.GetScripture());
-
-        Console.ReadLine();
-        Console.Clear();
-        scriptureTest.HiddenWorddByIndex(scriptureTest._indexHiddenList[5]);
-
-        Console.WriteLine(referenceTest.GetReference()+" "+ scriptureTest.DisplayScriptureList() + "funciono");
-        Console.WriteLine(scriptureTest.GetHiddenIndex());
-        /* Console.WriteLine(scriptureTest.GetWordFromList(3)); */
-        Console.WriteLine(scriptureTest.ListaAleatoria());
+        int i = 0;        
+        string _finish = "";
+        do {
+            _finish = Console.ReadLine();
+            if (_finish == "back") {                
+                scriptureTest.wordrestore();
+                Console.Clear();
+                Console.WriteLine(referenceTest.GetReference()+" "+ scriptureTest.DisplayScriptureList()); 
+                i--;
+            } else {
+            Console.Clear();
+            scriptureTest.CreateHiddenWordIndex(i);
+            Console.WriteLine(referenceTest.GetReference()+" "+ scriptureTest.DisplayScriptureList()); 
+                       
+            i++;
+            }
+        }
+        while(scriptureTest._hiddenWord_List_index.Count < scriptureTest.Get_wordys_Count() && _finish != "quit");       
         
         
         }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -6,37 +6,54 @@ using System.Xml.XPath;
 class Scripture{
     private string _newScripture;
     private List<Word> _wordys = new List<Word>{};
-    public int _indexOfHiddenWord = 0;
+    public int _indexOfHiddenWord = 5;
 
-    public List<int> _indexHiddenList = new List<int>{};
+    public List<int> _aleatoryList = new List<int>{};
 
     public Scripture(string scripture){
         _newScripture = scripture;   
         SeparateListAndAddToWordObject();
-        CreateIdexHiddenList();             
-        RandomIndex();
+        CreateAleatoryList();             
+        RandomIndex();        
     }
 
-    public void CreateIdexHiddenList(){
-        _indexHiddenList = Enumerable.Range(0, _wordys.Count).ToList();
+    public void CreateAleatoryList(){
+        //_aleatoryList = Enumerable.Range(0, _wordys.Count).ToList();
+        _aleatoryList = Enumerable.Range(0, _wordys.Count).ToList();
+        //_aleatoryList = Enumerable.Range(0, _wordys.Count)  // Genera todos los índices
+                             //.OrderBy(x => new Random().Next())  // Los desordena aleatoriamente
+          //                   .Take(_wordys.Count / 2)  // Toma la mitad
+            //                 .ToList();
+    }
+
+    public List<int> _hiddenWord_List_index = new List<int>();
+
+    public void CreateHiddenWordIndex(int i){        
+        _hiddenWord_List_index.Add(_aleatoryList[i]);   
     }
 
     public void RandomIndex(){
-        int finalIndex = _indexHiddenList.Count()-1;
+        int finalIndex = _aleatoryList.Count()-1;
         while(finalIndex > 0){
-            int temporalIndex = _indexHiddenList[finalIndex];
+            int temporalIndex = _aleatoryList[finalIndex];
             int randomIndex = new Random().Next(0, finalIndex);
-            _indexHiddenList[finalIndex] = _indexHiddenList[randomIndex];
-            _indexHiddenList[randomIndex] = temporalIndex;
+            _aleatoryList[finalIndex] = _aleatoryList[randomIndex];
+            _aleatoryList[randomIndex] = temporalIndex;
             finalIndex--;
+        }}        
 
-        }}
         public string ListaAleatoria(){
             
-            return string.Join(",",_indexHiddenList);
+            return string.Join(",",_aleatoryList);
         }
+
+        public string ListaIndexEsconder(){
             
-        
+            return string.Join(",",_hiddenWord_List_index);
+        }   
+        public void wordrestore(){
+            _hiddenWord_List_index.RemoveAt(_hiddenWord_List_index.Count-1);
+        }
 
     public string GetScripture(){
         return _newScripture;
@@ -49,19 +66,16 @@ class Scripture{
         }
         }
 
-
-        public void HiddenWorddByIndex(int index){
-            if (index < 0 && index >= _wordys.Count){
-            _indexOfHiddenWord = index;}
-        }
-public int GetHiddenIndex(){
-        return _indexOfHiddenWord;
+        
+public int Get_wordys_Count(){
+        return _wordys.Count;
     }    
 
-    public string DisplayScriptureList(){ 
+    public string DisplayScriptureList(){          
         string result = "";        
       for (int id = 0; id < _wordys.Count; id++){        
-        if (id == _indexOfHiddenWord ){
+        if(_hiddenWord_List_index.Contains(id)){
+        //if (id == _indexOfHiddenWord ){       
             result += _wordys[id].GetUnderscore() + " ";
         }
         else {
@@ -74,14 +88,14 @@ public int GetHiddenIndex(){
     //private List<String> _words = new List<String>{};
     
 // this code is just to test if the list is of objects is created, if the undercore version is created, and f I can get the object acording with the index
-    /* public string GetWordFromList(int index){
+    public string GetWordFromList(int index){
         string result2 = "";
         string result3 = "";
         result2 = _wordys[index].GetWord();
         result3 = _wordys[index].GetUnderscore();
 
         return result2 + result2.Length + result3;
-    } */
+    }
     
     
 
